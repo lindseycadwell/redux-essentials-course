867dab8a8ee70b021282a069bb8a88c64d9fdec2
Lindsey <lindseycadwell.dev@gmail.com>
2021-08-18 15:23:59 -0400

    Implemented post reactions and 'time ago' feature

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import { PostAuthor } from "./PostAuthor";
+import { TimeAgo } from "./TimeAgo";
+import { ReactionButtons } from "./ReactionButtons";
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params;
@@ -27,9 +29,13 @@ export const SinglePostPage = ({ match }) => {
         <p>
           <PostAuthor userId={post.userId} />
         </p>
+        <p>
+          <TimeAgo timestamp={post.date} />
+        </p>
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
+        <ReactionButtons post={post} />
       </article>
     </section>
   );
